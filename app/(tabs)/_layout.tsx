e4fac2af25f2067055f2657204916b2f6afa85a8
Higiniecito <cristian.ramirez@clovit.com>
2025-02-13 16:23:23 -0500

    Add style to the tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,32 @@
 import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="about" options={{ title: 'About' }} />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+        headerStyle: {
+          backgroundColor: 'tomato',
+        },
+        headerShadowVisible: false,
+        headerTintColor: 'white',
+        tabBarStyle: {
+          backgroundColor: 'mintcream',
+        }
+      }}
+    >
+      <Tabs.Screen name="index" options={{
+        title: 'Home', tabBarIcon: ({ color, focused }) => (
+          <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+        )
+      }} />
+      <Tabs.Screen name="about" options={{
+        title: 'About', tabBarIcon: ({ color, focused }) => (
+          <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
+        )
+      }} />
     </Tabs>
   );
 }
