524e7839c249ed91aeacd3522a8396084daf335a
Higiniecito <cristian.ramirez@clovit.com>
2025-02-21 18:18:19 -0500

    Styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,42 +1,28 @@
-import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
-// import FilePickerComponent from "../components/FilePicker";
-import SignatureComponent from "../components/Signature";
-import SignatureWeb from "../components/SignatureWeb";
-// import StorageComponent from "../components/Storage";
-// import CameraComponent from "../components/Camera";
-// import LateralScrollTable from "../components/LateralScrollTable";
-// import ButtonFetch from "../components/Fetch";
-// import ButtonAxios from "../components/axios";
 
 export default function Index() {
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Texto</Text>
-      {/* <Link href="/about" style={styles.button}>Ir a About</Link> */}
-      {/* <Link href="/error" style={styles.button}>Ir a 404</Link> */}
-      {/* <LateralScrollTable /> */}
-      {/* <ButtonFetch /> */}
-      {/* <ButtonAxios /> */}
-      {/* <CameraComponent /> */}
-      {/* <StorageComponent /> */}
-      {/* <FilePickerComponent /> */}
-      {/* <SignatureComponent /> */}
-      <SignatureWeb />
+    <View style={styles.container}>
+      <Text style={styles.red}>red</Text>
+      <Text style={styles.bigBlue}>bigBlue</Text>
+      <Text style={[styles.bigBlue, styles.red]}>bigBlue, the red</Text>
+      <Text style={[styles.red, styles.bigBlue]}>red, the bigBlue</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
+  container: {
+    margin: 20,
+    marginTop: 50,
+  },
+  bigBlue: {
+    color: "blue",
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  red: {
+    color: "red",
   },
 });
