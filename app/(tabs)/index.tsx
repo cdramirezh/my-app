81d6580230e32358b67026e81ddf8712790428d5
Higiniecito <cristian.ramirez@clovit.com>
2025-02-21 21:22:33 -0500

    DirectionLayout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,102 @@
-import { StyleSheet, View } from "react-native";
+import React, {useState} from 'react';
+import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import type {PropsWithChildren} from 'react';
 
-export default function Index() {
+const DirectionLayout = () => {
+  const [direction, setDirection] = useState<'ltr' | 'rtl'>('ltr');
 
   return (
-    <View style={styles.container}>
-      <View style={[styles.box1]} />
-      <View style={[styles.box2]} />
-      <View style={[styles.box3]} />
-    </View>
+    <PreviewLayout
+      label="Dirección"
+      selectedValue={direction}
+      values={['ltr', 'rtl']}
+      setSelectedValue={setDirection}>
+      <View style={[styles.box, {backgroundColor: 'powderblue'}]} />
+      <View style={[styles.box, {backgroundColor: 'skyblue'}]} />
+      <View style={[styles.box, {backgroundColor: 'steelblue'}]} />
+      <Text style={{direction: 'rtl'}}>Cómo funciona un texto?</Text>
+    </PreviewLayout>
   );
-}
+};
 
-const box = {
-  with: 75,
-  height: 75,
-}
+type PreviewLayoutProps = PropsWithChildren<{
+  label: string;
+  values: ('ltr' | 'rtl')[];
+  selectedValue: 'ltr' | 'rtl';
+  setSelectedValue: (value: 'ltr' | 'rtl') => void;
+}>;
+
+const PreviewLayout = ({
+  label,
+  children,
+  values,
+  selectedValue,
+  setSelectedValue,
+}: PreviewLayoutProps) => (
+  <View style={{padding: 10, flex: 1}}>
+    <Text style={styles.label}>{label}</Text>
+    <View style={styles.row}>
+      {values.map(value => (
+        <TouchableOpacity
+          key={value}
+          onPress={() => setSelectedValue(value)}
+          style={[styles.button, selectedValue === value && styles.selected]}>
+          <Text
+            style={[
+              styles.buttonLabel,
+              selectedValue === value && styles.selectedLabel,
+            ]}>
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+    <View style={[styles.container, {direction: selectedValue}]}>{children}</View>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 8,
+    backgroundColor: 'aliceblue',
   },
-  box1: {
-    ...box,
-    backgroundColor: "salmon",
+  box: {
+    width: 50,
+    height: 50,
   },
-  box2: {
-    ...box,
-    backgroundColor: "darkorange",
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
-  box3: {
-    ...box,
-    backgroundColor: "green",
+  button: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: 'oldlace',
+    alignSelf: 'flex-start',
+    marginHorizontal: '1%',
+    marginBottom: 6,
+    minWidth: '48%',
+    textAlign: 'center',
+  },
+  selected: {
+    backgroundColor: 'coral',
+    borderWidth: 0,
+  },
+  buttonLabel: {
+    fontSize: 12,
+    fontWeight: '500',
+    color: 'coral',
+  },
+  selectedLabel: {
+    color: 'white',
+  },
+  label: {
+    textAlign: 'center',
+    marginBottom: 10,
+    fontSize: 24,
   },
 });
+
+export default DirectionLayout;
